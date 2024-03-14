f7c4706d75af8eb2879b90f09055010e207bb490
Benoit22324 <sunbenoit384@gmail.com>
2024-03-14 15:43:09 +0100

    Ajout du prototype d'auto-complétion

--- src/components/InputCity.jsx
+++ src/components/InputCity.jsx
@@ -1,11 +1,28 @@
 import { useWeatherContext } from "../utils/WeatherContext";
+import axios from "axios";
 
 const InputCity = () => {
     const [state, dispatch] = useWeatherContext();
 
+    const propos = async (word) => {
+        try {
+            const response = await axios.get('https://api-adresse.data.gouv.fr/search/?q=' + word + '&autocomplete=1')
+            const data = response.data.features
+            console.log(data[0].properties.city)
+        }
+        catch {
+            console.log('erreur de propos')
+        }
+    }
+
+    const update = (e) => {
+        dispatch({type: 'updateValue', payload: e.target.value})
+        if (state.input.length > 2 && state.input !== '') propos(state.input)
+    }
+
     return (
         <>
-            <label>Entrez le nom d'une ville: <input type="text" style={{padding: '5px', borderRadius: '5px'}} value={state.input} onChange={(e) => dispatch({type: 'updateValue', payload: e.target.value})}/></label>
+            <label>Entrez le nom d'une ville: <input type="text" style={{padding: '5px', borderRadius: '5px'}} value={state.input} onChange={update}/></label>
         </>
     )
 }
